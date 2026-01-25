1824742319d43f79507bbd81a23bcb6e2c1989a3
Ddinhgiang <giangauto2603@gmail.com>
2026-01-25 20:20:40 +0700

    Initial commit

---
+++ testcases/com/nopcommerce/users/User_01_Register_Login.java
@@ -0,0 +1,29 @@
+package com.nopcommerce.users;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class User_01_Register_Login {
+    WebDriver driver;
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.nopcommerce.com/");
+    }
+    @Test
+    public void TC_01_Register() {
+
+    }
+    @Test
+    public void TC_02_Login() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+}
